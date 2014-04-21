dfb9384408fe701f6511e12c0b46cd596d0baa5f
sevensyd <sevensyd@gmail.com>
2014-04-21 16:06:36 +1000

    test again

--- test/src/test/Hello.scala
+++ test/src/test/Hello.scala
@@ -12,6 +12,9 @@ object Hello {
     println(c.acc)
     val u = cc
     println(c.acc)
+    // test
+    // test2
+    // test2
 
     val scCalc = new ScientificCalculator("TI")
     println(scCalc.color)
